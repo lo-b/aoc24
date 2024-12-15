0a8263b40b70cb3aeb2fd4b3ba27a9210ac79258
lo-b <bram_lo@outlook.com>
2024-12-15 21:53:05 +0100

    feat: solution part 2
    Quick, dirty & lazy solution to day 2

--- cmd/mull-it-over/solution/main.go
+++ cmd/mull-it-over/solution/main.go
@@ -9,6 +9,9 @@ import (
 	"github.com/lo-b/aoc24/internal/puzzleio"
 )
 
+// NOTE: for mul to be 'valid' it has to be preceded by neither 'do' or 'don't'
+// instructions or be preceded by the latest 'do' op which index is > don't
+// op's index.
 func main() {
 	puzzleInput, err := puzzleio.NewPuzzleInput("assets/corrupted_memory_log.txt")
 	if err != nil {
@@ -27,21 +30,28 @@ func main() {
 	reader := puzzleInput.Reader
 
 	var totalSum int
+	var extendedTotalSum int
+	var allLines string
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			break
 		}
-		lineSum := Parse(line)
-		totalSum += lineSum
+
+		allLines += line
 	}
+
+	lineSum, extendedMulSum := Parse(allLines)
+	totalSum = lineSum
+	extendedTotalSum = extendedMulSum
 	fmt.Printf("Total sum of 'mul' expressions: %d\n", totalSum)
+	fmt.Printf("Total sum of 'mul' do/don't extended: %d\n", extendedTotalSum)
 }
 
 // Parse interprets valid multiplication expressions and returns their sum. A
 // valid multiplication expression is defined as 'mul(X,Y)' where X, Y are ints
 // in range [-999, 999].
-func Parse(line string) int {
+func Parse(line string) (int, int) {
 	const Expression = "mul("
 	const Seperator = ','
 	const EndChar = ')'
@@ -51,11 +61,38 @@ func Parse(line string) int {
 	offsets := indexedLine.Lookup([]byte(Expression), -1)
 
 	var mulSum int
+	var extendedMulSum int
 	for _, offset := range offsets {
+		// mulIsEnabled := offsetEnabled(line, offset)
 		mulSum += TryMulOperation(line, offset, Expression, Seperator, EndChar)
+
+		if offsetEnabled(line, offset) {
+			extendedMulSum += TryMulOperation(line, offset, Expression, Seperator, EndChar)
+		}
 	}
 
-	return mulSum
+	return mulSum, extendedMulSum
+}
+
+func offsetEnabled(line string, offset int) bool {
+	prevSlice := line[:offset]
+	doIdx, dontIdx := strings.LastIndex(prevSlice, "do()"), strings.LastIndex(prevSlice, "don't()")
+
+	// NOTE: no do/dont prev op found (standard enabled) => return true
+	if doIdx == -1 && dontIdx == -1 {
+		return true
+	}
+
+	// NOTE: no do(), don't() called before
+	if doIdx == -1 && dontIdx > 0 {
+		return false
+	}
+
+	if doIdx > dontIdx {
+		return true
+	}
+
+	return false
 }
 
 func TryMulOperation(line string, offset int, operation string, seperator rune, endChar rune) int {
